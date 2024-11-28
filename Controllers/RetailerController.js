a4d39a956afb0a60585f65ba0489e000f117fb64
anil gummula <anilgummula2060@gmail.com>
2024-11-29 01:09:19 +0530

    updated

--- Controllers/RetailerController.js
+++ Controllers/RetailerController.js
@@ -4,7 +4,7 @@ const Order = require('../models/Order');
 // Get all products listed by farmers
 exports.getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find().populate('farmer', 'username');
+        const products = await Product.find();
         console.log('Products retrieved:', products);
         res.json(products); // Ensure products are sent as an object with `products` key
       } catch (error) {
