57446aedf5c5d2396c348f3ff71019e9574cfc9f
anil gummula <anilgummula2060@gmail.com>
2024-12-01 01:26:34 +0530

    updated

--- Controllers/FarmerController.js
+++ Controllers/FarmerController.js
@@ -1,12 +1,14 @@
-// const User = require('../models/user');
+const User = require('../models/user');
 const Product = require('../models/Product');
 const Order = require('../models/Order'); // Ensure Order model is required if used in manageOrder
 
 // Fetch products listed by the farmer
 exports.getMyProducts = async (req, res) => {
   try {
-    const products = await Product.find({ farmer: req.user._id });
-    res.status(200).json({ success: true, products });
+    const products = await Product.find({ farmer : req.user._id });
+    const product = await Product.findOne({farmer : req.user._id});
+    const farmerDetails =await User.findById(product.farmer);
+    res.status(200).json({ success: true, products ,farmerDetails : farmerDetails});
   } catch (err) {
     res.status(500).json({ success: false, message: 'Failed to fetch products!', error: err.message });
   }
@@ -66,8 +68,16 @@ exports.manageOrder = async (req, res) => {
         product.quantity += orderQunatity;
         product.save();
     }
+    if(status === "Accepted"){
+        const productId = order.product;
+        const product = await Product.findById(productId);
+        const orderQunatity = order.quantity;
+        const earnings = (orderQunatity) * (product.price);
+        const user = await User.findById(order.farmer);
+        user.myEarnings +=earnings; 
+    }
 
-    if (!order) return res.status(404).json({ success: false, message: 'Order not found!' });
+    if (!order) return res.status(404).json({ success: false, message: 'Order not found!' , earnings : earnings});
 
     order.status = status;
     await order.save();
