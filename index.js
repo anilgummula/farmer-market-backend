146681bdd1fbb96fd215bd8669834ff870c6ff1b
anil gummula <anilgummula2060@gmail.com>
2024-12-01 16:55:15 +0530

    updated

--- index.js
+++ index.js
@@ -13,30 +13,17 @@ require('./models/db.js'); // Initialize database connection
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use((err, req, res, next) => {
-    if (err instanceof multer.MulterError) {
-      // Handle multer-specific errors
-      return res.status(400).json({ message: err.message });
-    } else if (err) {
-      // Handle other errors
-      return res.status(400).json({ message: err.message });
-    }
-    next();
-  });
+// app.use((err, req, res, next) => {
+//     if (err instanceof multer.MulterError) {
+//       // Handle multer-specific errors
+//       return res.status(400).json({ message: err.message });
+//     } else if (err) {
+//       // Handle other errors
+//       return res.status(400).json({ message: err.message });
+//     }
+//     next();
+//   });
   
-
-
-// Ensure 'uploads/products' directory exists
-// const uploadDir = path.join(__dirname, 'uploads/products');
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir, { recursive: true });
-//   console.log(`Directory ${uploadDir} created.`);
-// }
-
-// Serve static files from 'uploads' directory
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
 
 // Middleware setup
 app.use(express.json())
