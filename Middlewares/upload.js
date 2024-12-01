f54887542aef185353906f902172064dfc9f351d
anil gummula <anilgummula2060@gmail.com>
2024-12-01 18:02:01 +0530

    updated

--- Middlewares/upload.js
+++ Middlewares/upload.js
@@ -3,16 +3,16 @@ const path = require('path');
 const fs = require('fs');
 
 // Ensure the 'uploads/images/' directory exists
-const uploadDir = path.join(__dirname, '../uploads/images');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-  console.log(`Directory ${uploadDir} created.`);
-}
+// const uploadDir = path.join(__dirname, '../uploads/images');
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+//   console.log(`Directory ${uploadDir} created.`);
+// }
 
 // Multer configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, '/tmp/uploads');
   },
   filename: (req, file, cb) => {
     const uniqueName = `${Date.now()}-${file.originalname}`;
@@ -20,20 +20,9 @@ const storage = multer.diskStorage({
   },
 });
 
-// File filter for image validation
-const fileFilter = (req, file, cb) => {
-  if (['image/jpeg', 'image/png', 'image/jpg'].includes(file.mimetype)) {
-    cb(null, true); // Accept the file
-  } else {
-    cb(new Error('Only JPEG and PNG images are allowed'), false); // Reject the file
-  }
-};
 
-// Multer setup with storage, limits, and file filter
-const upload = multer({
-  storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
-  fileFilter,
-});
+
+// Multer setup with storage=
+const upload = multer({ storage: storage })
 
 module.exports = upload;
