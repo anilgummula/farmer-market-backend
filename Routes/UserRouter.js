13d4c64bf471fa7833bacbf9a0085c39205c2427
anil gummula <anilgummula2060@gmail.com>
2024-12-04 00:35:03 +0530

    updated

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { getProfile, updateProfile } = require('../Controllers/UserController');
 const { ensureAuthenticated } = require('../Middlewares/Auth');
 
-router.get('/view', ensureAuthenticated, getProfile);
+// router.get('/view', ensureAuthenticated, getProfile);
 router.put('/update', ensureAuthenticated, updateProfile);
 
 module.exports = router;
