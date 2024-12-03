79a6f1c5b1a56f6a78805ad26fc5310a409933f0
anil gummula <anilgummula2060@gmail.com>
2024-12-03 23:27:35 +0530

    updated

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -2,8 +2,8 @@ const User = require('../models/user');
 
 exports.getProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.user.id);
-    res.json(user);
+    const user = await User.findById(req.user._id);
+    res.status(200).json({message : "profile loaded successfully" ,user : user});
   } catch (err) {
     res.status(500).json({ message: 'Error fetching profile' });
   }
@@ -12,8 +12,8 @@ exports.getProfile = async (req, res) => {
 exports.updateProfile = async (req, res) => {
   try {
     const { username, email, address } = req.body;
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, { username, email, address }, { new: true });
-    res.json(updatedUser);
+    const updatedUser = await User.findByIdAndUpdate(req.user._id, { username, email, address }, { new: true });
+    res.status(200).json({message : "Profile updation success",updatedUser});
   } catch (err) {
     res.status(500).json({ message: 'Error updating profile' });
   }
